78aac6f36fe73d086d5e219cf53c8eb5071fa02a
giles knap <gilesknap@gmail.com>
2024-11-30 16:30:33 +0000

    update example launcher image version

--- services/example-bl01t.sh
+++ services/example-bl01t.sh
@@ -11,5 +11,5 @@ fi
 ca="-p 127.0.0.1:5064:5064/udp -p 127.0.0.1:5064-5065:5064-5065"
 pva="-p 127.0.0.1:5076:5076/udp -p 127.0.0.1:5075:5075"
 args="--rm -it"
-image="ghcr.io/epics-containers/ioc-adsimdetector-demo:2024.9.1"
+image="ghcr.io/epics-containers/ioc-adsimdetector-demo:2024.11.1"
 $docker run $args $ca $pva $image
