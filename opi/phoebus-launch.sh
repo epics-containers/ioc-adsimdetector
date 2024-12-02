b284b6b714ca27ac5cd5ce1de25e481777d27f91
Giles Knap <giles.knap@diamond.ac.uk>
2024-12-02 09:49:37 +0000

    restore default ports in devcontainer.json

--- opi/phoebus-launch.sh
+++ opi/phoebus-launch.sh
@@ -9,9 +9,9 @@ workspace=$(realpath ${thisdir}/..)
 source ${workspace}/.devcontainer/.env
 cat ${workspace}/opi/settings.ini |
     sed -r \
-    -e "s|5064|${EPICS_CA_SERVER_PORT}|" \
-    -e "s|5075|${EPICS_PVA_SERVER_PORT}|" \
-    -e "s|5065|${EPICS_CA_REPEATER_PORT}|" > /tmp/settings.ini
+    -e "s|5064|${EPICS_CA_SERVER_PORT:-5064}|" \
+    -e "s|5075|${EPICS_PVA_SERVER_PORT:-5075}|" \
+    -e "s|5065|${EPICS_CA_REPEATER_PORT:-5065}|" > /tmp/settings.ini
 
 settings="
 -resource ${workspace}/opi/bl01t-ea-ioc-02.bob
