840c3cf6bec6152ed213458f7aa2503825398334
giles knap <giles.knap@diamond.ac.uk>
2025-11-24 13:28:35 +0000

    new start.sh fully working

--- ioc/start.sh
+++ ioc/start.sh
@@ -3,21 +3,17 @@
 # log commands and stop on errors
 set -xe
 
-# wrap this script in stdio-expose if running in Kubernetes
-# this allows the IOC to expose its console on a Unix socket
+# wrap the console *************************************************************
+
 if [[ -n ${KUBERNETES_PORT} && -z ${STDIO_EXPOSED} ]]; then
-    STDIO_EXPOSED=YES exec stdio-expose ${IOC}/start.sh
+    STDIO_EXPOSED=YES exec stdio-socket ${IOC}/start.sh
     exit 0
 fi
 
 # error reporting **************************************************************
 
 function ibek_error {
-    local error_code=$?
-    local error_line=$BASH_LINENO
-    local error_command=$BASH_COMMAND
-    echo "Error on line $error_line: $error_command (exit code: $error_code)"
-    echo "$1"
+    echo "Error on line $BASH_LINENO: $BASH_COMMAND (exit code: $?)"
 
     # Wait for a bit so the container does not exit and restart continually
     sleep 120
@@ -30,20 +26,20 @@ trap ibek_error ERR
 
 cd ${IOC}
 
-CONFIG_DIR=${IOC}/config
-RUNTIME_DIR=${EPICS_ROOT}/runtime
+export CONFIG_DIR=${IOC}/config
+export RUNTIME_DIR=${EPICS_ROOT}/runtime
+mkdir -p ${RUNTIME_DIR}
 
 # add module paths to environment for use in ioc startup script
 if [[ -f ${SUPPORT}/configure/RELEASE.shell ]]; then
     source ${SUPPORT}/configure/RELEASE.shell
 fi
 
-mkdir -p ${RUNTIME_DIR}
-
 # check for an override start.sh script ****************************************
 
 if [ -f ${CONFIG_DIR}/start.sh ]; then
     exec bash ${CONFIG_DIR}/start.sh
+fi
 
 # copy hand coded files to runtime folder **************************************
 
@@ -68,8 +64,7 @@ ibek runtime generate-autosave
 # build expanded database using msi
 if [ -f ${RUNTIME_DIR}/ioc.subst ]; then
     includes=$(for i in ${SUPPORT}/*/db; do echo -n "-I $i "; done)
-    bash -c "msi -o${RUNTIME_DIR}/ioc.db ${includes} -I${RUNTIME_DIR}
-             -S${RUNTIME_DIR}/ioc.subst"
+    bash -c "msi -o${RUNTIME_DIR}/ioc.db ${includes} -I${RUNTIME_DIR} -S${RUNTIME_DIR}/ioc.subst"
 fi
 
 # Launch the IOC ***************************************************************
